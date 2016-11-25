4b96534d40dd4511a70f88076a73466daca26afb
dmitry <dimdi-y@ya.ru>
2016-11-25 23:55:21 +0300

    Added a simple example of how AJAX API works

--- JS/API.jsx
+++ JS/API.jsx
@@ -2,19 +2,20 @@
 import axios from 'axios';
 import properties from './settings.jsx';
 
-module.exports = {
-	request : function  (url, data={}, method='get') {
-		url = properties.location+'/API/' + url + '.php';
-		
-		let config = {
-			method : method,
-			url : url,
-			data : data
-		}
 
-		if (method === 'get'){
-			config['params'] = data;
-		}
-		return axios(config);
+//requests from API/$url.php, sending $data, using $method
+//returns a promise, wich returns the response  
+export default function request (url, data={}, method='get') {
+	url = properties.location+'/API/' + url + '.php';
+		
+	let config = {
+			method : method,
+		url : url,
+			data : data
 	}
+
+	if (method === 'get'){
+		config['params'] = data;
+		}
+	return axios(config);
 }
--- JS/index.jsx
+++ JS/index.jsx
@@ -1,38 +1,43 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import axios from 'axios';
-import API from './API.jsx'
+import request from './API.jsx'
 
 import properties from './settings.jsx';
 
+
+var outVal = 0; 
 
 const app = document.getElementById("app");
 
 class Layout extends(React.Component){
 	constructor(props){
 		super(props);
-
-		this.state = {
-			userName : ""
-		};
+		this.state = {val : 0};
 	}
-
-	componentDidMount(){
-
+	
+	componentWillMount(){
+		let ok_btn = document.getElementsByName('OK')[0];
+		ok_btn.addEventListener("click", e=>{
+			let b = document.getElementsByName('B')[0].value;
+			let a = document.getElementsByName('A')[0].value;
+			request('sum', {'a':a, 'b':b}).then(res => {
+				//console.log(this);
+				this.setState({val : res.data.response});
+				this.render();
+			});
+			e.preventDefault();
+		});
 	}
 
 	render(){
 		return (
-			<h1>Hello, {this.state.userName + this.state.userName?"!":""}</h1>
+			<h1>Hello, {this.state.val}</h1>
 			);
 	}
 }
 
 
-console.log(API.request)
-API.request('sum', {'a':10, 'b':25.5}).then(res => {
-	console.log(res);
-})
 
-let layout = <Layout />;
-ReactDOM.render(layout, app);
+ReactDOM.render((
+	<Layout />
+	), app);
