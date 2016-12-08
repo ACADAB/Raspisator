5d222c488db41912f210aa6747f2a1c5a817e0af
dmitry <dimdi-y@ya.ru>
2016-12-08 23:51:52 +0300

    Now we don't need settings.jsx

--- JS/API.jsx
+++ JS/API.jsx
@@ -1,11 +1,10 @@
 import axios from 'axios';
-import properties from './settings.jsx';
 import queryString from 'query-string';
 
 //requests from API/$url.php, sending $data, using $method
 //returns a promise, wich returns the response  
 export default function request (url, data={}, method='get') {
-	url = properties.location+'/API/' + url + '.php';
+	url = document.location.pathname+'/API/' + url + '.php';
 		
 	let config = {
 			method : method,
