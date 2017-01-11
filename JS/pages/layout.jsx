fbaf08541d49b3d8c675cffd351dc8e6c799bddb
dmitry <dimdi-y@ya.ru>
2017-01-11 22:37:17 +0300

    Refactored navbar code, now the user name is displayed in navbar-right

--- JS/pages/layout.jsx
+++ JS/pages/layout.jsx
@@ -30,59 +30,59 @@ export default class Layout extends(React.Component){
 
 	render(){
 		let currentPath = this.props.routes[this.props.routes.length - 1].path;
+		let header = (<Navbar.Header>
+							<Navbar.Brand>
+								Raspisator
+							</Navbar.Brand>
+							<Navbar.Toggle />
+						</Navbar.Header>);
+		let menu = [];
+		let login = [];
 		if (this.state.account.isLoggedIn){
 			if (currentPath == undefined) currentPath = 'users';
-			return (	
-				<div>
-
-					<Grid>
-						<Row>
-							<h1>Hello, {this.state.account.name}</h1>
-							<Navbar>	
-								<Nav bsStyle="tabs" activeKey={currentPath} onSelect={this.handleSelect}>
-									<NavItem eventKey="home">Home</NavItem>
-									<NavItem eventKey="projects">Projects</NavItem>
-								</Nav>
-								<Nav pullRight onSelect={this.handleSelect}>
-									<NavItem eventKey="logout">Logout</NavItem>
-								</Nav>
-							</Navbar>
-					
-					
-
-							{this.props.children}
-						</Row>
-					</Grid>
-				</div>
-			);
+			menu = [
+				<NavItem eventKey="home" key={0}>Home</NavItem>,
+				<NavItem eventKey="projects" key={1}>Projects</NavItem>
+			];
+			login = [
+				<NavItem key={0}>Hello, {this.state.account.name}</NavItem>,
+				<NavItem key={1} eventKey="logout">Logout</NavItem>
+			];
 		}
 		else{
 			if (currentPath ==  undefined) currentPath = 'login';
-			return (
+			menu = [
+				<NavItem key={0} eventKey="home">Home</NavItem>
+			];
+			login = [
+				<NavItem key={0} eventKey="register">Register</NavItem>,
+				<NavItem key={1} eventKey="login">Log in </NavItem>
+			];
+		}
+
+		return (	
 				<div>
 
 					<Grid>
 						<Row>
-							<h1>Hello, {this.state.account.name}</h1>
-							<Navbar>	
-								<Nav bsStyle="tabs" activeKey={currentPath} onSelect={this.handleSelect}>
-									<NavItem eventKey="home">Home</NavItem>
-								</Nav>
-								<Nav pullRight onSelect={this.handleSelect}>
-									<NavItem eventKey="register">Register</NavItem>
-									<NavItem eventKey="login">Log in </NavItem>
-								</Nav>
+							<Navbar>
+								{header}
+								<Navbar.Collapse>
+									<Nav bsStyle="tabs" activeKey={currentPath} onSelect={this.handleSelect}>
+										{menu}
+									</Nav>
+									<Nav pullRight onSelect={this.handleSelect}>
+										{login}
+									</Nav>
+								</Navbar.Collapse>
 							</Navbar>
-
-
-
-
+					
+					
 
 							{this.props.children}
 						</Row>
 					</Grid>
 				</div>
 			);
-		}
 	}
 }
