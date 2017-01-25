bbc46ae674d4d8d8f41b436be0b9523736f78c7f
Natik <Natik>
2017-01-26 01:33:33 +0300

    russian calendart

--- JS/pages/logout.jsx
+++ JS/pages/logout.jsx
@@ -3,12 +3,25 @@ import React from "react";
 import request from '../API.jsx'
 import * as AccountActions from '../actions/accountActions.jsx';
 import {Link, hashHistory} from 'react-router';
-import {Button} from 'react-bootstrap';
+import FormData from 'react-form-data';
+import {Form, Modal, Button, FormGroup, FormControl, Col, ControlLabel} from 'react-bootstrap';
+import AccountStore from '../stores/accountStore.jsx';
+import renderAlert from './alert.jsx';
+
 
 export default class Logout extends(React.Component){
 	constructor(props){
 		super(props);
 		//this.mixins = [Router.Navigation]
+		this.state = { showModal: false,  alertMessage: ''};
+		this.formData = defData;
+		this.close = this.close.bind(this);
+        this.open = this.open.bind(this);
+        this.alert = this.alert.bind(this);
+	}
+	
+	alert(isLoggedIn){
+		this.setState({showModal: this.state.showModal, alertMessage: 'Неверная пара логин/пароль'});
 	}
 	
 	logout(){
@@ -18,13 +31,24 @@ export default class Logout extends(React.Component){
 	goBack(){
 		hashHistory.goBack();
 	}
+	
+	close() {
+        this.setState({ showModal: false , alertMessage: ''});
+        this.formData = Object.create(defData);
+    }
+
+    open() {
+        this.setState({ showModal: true , alertMessage:''});
+    }
 
 	render(){
 		return (
 			<div>
-				<h1>Are you sure you want to log out?</h1>
-				<Button bsStyle='danger' onClick={this.logout.bind(this)}>Log out!</Button>
-				<Button onClick={this.goBack.bind(this)}>no</Button>
+				<Modal show={this.state.showModal} onHide={this.close}>
+					<h1>Are you sure you want to log out?</h1>
+					<Button bsStyle='danger' onClick={this.logout.bind(this)}>Log out!</Button>
+					<Button onClick={this.goBack.bind(this)}>no</Button>
+				</Modal>
 			</div>
 			);
 
